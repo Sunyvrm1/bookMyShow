d8fd5c6fcb2b6c1e370a21fe881edfeffc656d94
Sushil Verma <115399549+Sunyvrm1@users.noreply.github.com>
2024-04-12 16:12:58 +0530

    16:12

---
+++ Dist/js/movies.js
@@ -0,0 +1,40 @@
+const storedImageId = localStorage.getItem("clickImageId");
+const storedImageAPI = localStorage.getItem("clickImageAPI");
+console.log(storedImageId);
+console.log(storedImageAPI);
+
+function displayMovies(storedID, storedAPI) {
+  fetch(storedAPI)
+    .then((res) => res.json())
+    .then((data) => {
+      const matchingId = data.results.find(
+        (item) => item.id.toString() === storedID
+      );
+
+      if(matchingId) {
+        const bannerPoster = document.getElementById("bannerPoster");
+        const bannerTitle = document.getElementById("bannerTitle");
+        const bannerRating = document.getElementById("bannerRating");
+        const bannerVote = document.getElementById("bannerVote");
+        const aboutText = document.getElementById("aboutText");
+        const bannerMovies = document.querySelector(".bannerMovies");
+
+        bannerPoster.src = "https://image.tmdb.org/t/p/w500/" + matchingId.poster_path;
+        bannerTitle.innerHTML = matchingId.name || matchingId.title;
+        bannerRating.innerHTML = matchingId.vote_average.toFixed(1);
+        bannerVote.innerHTML = matchingId.vote_count;
+        aboutText.innerHTML = matchingId.overview;
+        bannerMovies.style.backgroundImage = `linear-gradient(
+            90deg,
+            rgba(26, 26, 26, 1),
+            rgba(26, 26, 26, 1),
+            rgba(26, 26, 26, 1),
+            rgba(26, 26, 26, 0.4),
+            rgba(26, 26, 26, 1),
+            rgba(26, 26, 26, 1)
+          ), url(https://image.tmdb.org/t/p/w500/${matchingId.backdrop_path})`;
+      }
+    });
+}
+
+displayMovies(storedImageId, storedImageAPI);
