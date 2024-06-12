8ef59b2bed262a1f6e4aea12f1be2f9978f4c5fa
Sushil Verma <115399549+Sunyvrm1@users.noreply.github.com>
2024-06-12 07:37:50 +0000

    13:07

--- Dist/js/movies.js
+++ Dist/js/movies.js
@@ -11,19 +11,22 @@ function displayMovies(storedID, storedAPI) {
         (item) => item.id.toString() === storedID
       );
 
-      if(matchingId) {
+      if (matchingId) {
         const bannerPoster = document.getElementById("bannerPoster");
         const bannerTitle = document.getElementById("bannerTitle");
         const bannerRating = document.getElementById("bannerRating");
         const bannerVote = document.getElementById("bannerVote");
         const aboutText = document.getElementById("aboutText");
         const bannerMovies = document.querySelector(".bannerMovies");
+        const movieName = document.querySelector(".movieName");
 
-        bannerPoster.src = "https://image.tmdb.org/t/p/w500/" + matchingId.poster_path;
+        bannerPoster.src =
+          "https://image.tmdb.org/t/p/w500/" + matchingId.poster_path;
         bannerTitle.innerHTML = matchingId.name || matchingId.title;
         bannerRating.innerHTML = matchingId.vote_average.toFixed(1);
         bannerVote.innerHTML = matchingId.vote_count;
         aboutText.innerHTML = matchingId.overview;
+        movieName.innerHTML = matchingId.title;
         bannerMovies.style.backgroundImage = `linear-gradient(
             90deg,
             rgba(26, 26, 26, 1),
@@ -39,19 +42,27 @@ function displayMovies(storedID, storedAPI) {
 
 displayMovies(storedImageId, storedImageAPI);
 
-
 const ticketBook = document.querySelectorAll(".ticketBook");
 ticketBook.forEach((ticket) => {
   ticket.addEventListener("click", () => {
     const overlay = document.querySelector(".overlay");
     overlay.classList.remove("opacityHalf");
     document.querySelector("body").style.overflowY = "hidden";
-  })
-})
+  });
+});
 
 const closeIcon = document.querySelector(".closeIcon");
 closeIcon.addEventListener("click", () => {
   const overlay = document.querySelector(".overlay");
   overlay.classList.add("opacityHalf");
   document.querySelector("body").style.overflowY = "scroll";
-})
+});
+
+//book ticket button
+
+const LangButton = document.querySelectorAll(".LangButton");
+LangButton.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("Hello by SV");
+  });
+});
