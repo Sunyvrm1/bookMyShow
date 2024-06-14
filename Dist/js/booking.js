333b8effd5aacde55ed62f12e53b6b3ba6baa2e1
Sunyvrm1 <verma.suny1603@gmail.com>
2024-06-14 22:30:54 +0530

    22:30

--- Dist/js/booking.js
+++ Dist/js/booking.js
@@ -1,5 +1,7 @@
 const bookDateCont = document.querySelector(".bookDateCont");
 
+//date show
+
 fetch("date.json")
   .then((res) => res.json())
   .then((data) => {
@@ -21,6 +23,41 @@ fetch("date.json")
       btn.addEventListener("click", () => {
         bookDate.forEach((btn) => btn.classList.remove("bookDateActive"));
         btn.classList.add("bookDateActive");
+      });
+    });
+  });
+
+//time and location
+
+const bookCinema = document.querySelector(".bookCinema");
+
+fetch("places.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((item) => {
+      const html = `<div class="bookCinemaCont">
+                <div class="bookCinemaName">
+                    <p>${item.name}</p>
+                    <img src="./Dist/images/cinema.png" alt="">
+                </div>
+                <div class="bookCinemaTime">
+                    <ul>
+  
+                    </ul>
+                </div>
+            </div>`;
+      bookCinema.insertAdjacentHTML("beforeend", html);
+
+      const currentCinemaCont = bookCinema.lastElementChild;
+      const bookCinemaTime =
+        currentCinemaCont.querySelector(".bookCinemaTime ul");
+      item.time.forEach((time) => {
+        bookCinemaTime.insertAdjacentHTML(
+          "beforeend",
+          `
+          <li>${time}</li>
+          `
+        );
       });
     });
   });
